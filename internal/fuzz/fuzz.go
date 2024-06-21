5139fd9c2e52626afda5f3954d1e3dd0d97419f0
Kanstantsin Markelau <48592947+kmarkela@users.noreply.github.com>
2024-06-21 14:32:05 +0200

    fuzz-cmd

--- internal/fuzz/fuzz.go
+++ internal/fuzz/fuzz.go
@@ -1,1 +1,15 @@
 package fuzz
+
+import "github.com/kmarkela/duffman/internal/pcollection"
+
+type Fuzzer struct{}
+
+func New(workers, maxReq int,
+	headers, excludeParam, parameters []string,
+	fname, proxy string,
+	verbose bool) (Fuzzer, error) {
+
+	return Fuzzer{}, nil
+}
+
+func (f *Fuzzer) Run(col *pcollection.Collection) {}
