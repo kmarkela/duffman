c13bbf56fe8b74f301ac1b2c4c811f8b83adf9aa
Kanstantsin Markelau <48592947+kmarkela@users.noreply.github.com>
2024-06-21 15:51:10 +0200

    fuzzer

--- internal/fuzz/fuzz.go
+++ internal/fuzz/fuzz.go
@@ -1,15 +1,67 @@
 package fuzz
 
-import "github.com/kmarkela/duffman/internal/pcollection"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
 
-type Fuzzer struct{}
+	"github.com/kmarkela/duffman/internal/pcollection"
+)
 
-func New(workers, maxReq int,
-	headers, excludeParam, parameters []string,
-	fname, proxy string,
-	verbose bool) (Fuzzer, error) {
-
-	return Fuzzer{}, nil
+type Fuzzer struct {
+	workers, maxReq           int
+	excludeParams, parameters []string
+	verbose                   bool
+	headers                   map[string]string
+	tr                        *http.Transport
 }
 
-func (f *Fuzzer) Run(col *pcollection.Collection) {}
+func New(workers, maxReq int, headers, excludeParams, parameters []string, proxy string, verbose bool) (Fuzzer, error) {
+
+	var fuzzer = Fuzzer{}
+
+	// parse headers
+	h, err := pheaders(headers)
+	if err != nil {
+		return fuzzer, err
+	}
+
+	// parse proxy
+	proxyUrl, err := url.Parse(proxy)
+	if err != nil {
+		return fuzzer, err
+	}
+	return Fuzzer{
+		headers:       h,
+		workers:       workers,
+		maxReq:        maxReq,
+		excludeParams: excludeParams,
+		parameters:    parameters,
+		verbose:       v,
+		tr:            &http.Transport{Proxy: http.ProxyURL(proxyUrl)},
+	}, nil
+}
+
+func pheaders(headers []string) (map[string]string, error) {
+	rh := make(map[string]string)
+
+	for _, h := range headers {
+		p := strings.Split(h, ":")
+		if len(p) < 2 {
+			return nil, fmt.Errorf("%s is wrong header format", h)
+		}
+		rh[strings.TrimSpace(p[0])] = p[1]
+	}
+
+	return rh, nil
+}
+
+func (f *Fuzzer) Run(col *pcollection.Collection, fname string) {
+
+	// read wordlist
+	wordlist, err := pwlist(filename)
+	if err != nil {
+		return nil, err
+	}
+}
