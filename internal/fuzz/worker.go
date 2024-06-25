896c7e63e473593d2c248838c112264af9489a45
Kanstantsin Markelau <48592947+kmarkela@users.noreply.github.com>
2024-06-25 17:57:15 +0200

    start-worker

--- internal/fuzz/worker.go
+++ internal/fuzz/worker.go
@@ -16,21 +16,32 @@ func startWorker(wg *sync.WaitGroup, wq <-chan workUnit, tr *http.Transport) {
 
 			wu.r.Parameters.Get[wu.param] = wu.word
 
-			endpoint := fmt.Sprintf("%s?", wu.r.URL)
-			for gk, gv := range wu.r.Parameters.Get {
-				endpoint = fmt.Sprintf("%s%s=%s&", endpoint, gk, gv)
-			}
-
+			endpoint := createEndpoint(wu.r.URL, wu.r.Parameters.Get)
 			var r io.Reader = strings.NewReader(wu.r.Body)
 			doRequest(endpoint, r, tr)
 
 			continue
 		}
 
+		body := encodeBody(wu)
+		endpoint := createEndpoint(wu.r.URL, wu.r.Parameters.Get)
+		doRequest(endpoint, body, tr)
 	}
 
 	wg.Done()
+}
 
+func createEndpoint(url string, par map[string]string) string {
+	endpoint := fmt.Sprintf("%s?", url)
+	for gk, gv := range par {
+		endpoint = fmt.Sprintf("%s%s=%s&", endpoint, gk, gv)
+	}
+
+	return endpoint
 }
 
 func doRequest(endpoint string, body io.Reader, tr *http.Transport) {}
+
+func encodeBody(wu workUnit) io.Reader {
+	return nil
+}
