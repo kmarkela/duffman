d6999b8ab1bae73e88ea8bf6fca1c1642a8b0392
Kanstantsin Markelau <48592947+kmarkela@users.noreply.github.com>
2024-06-25 17:42:47 +0200

    start-worker

--- internal/fuzz/worker.go
+++ internal/fuzz/worker.go
@@ -1,10 +1,36 @@
 package fuzz
 
 import (
+	"fmt"
+	"io"
 	"net/http"
+	"strings"
 	"sync"
 )
 
 func startWorker(wg *sync.WaitGroup, wq <-chan workUnit, tr *http.Transport) {
 
+	for wu := range wq {
+
+		if !wu.parBody {
+
+			wu.r.Parameters.Get[wu.param] = wu.word
+
+			endpoint := fmt.Sprintf("%s?", wu.r.URL)
+			for gk, gv := range wu.r.Parameters.Get {
+				endpoint = fmt.Sprintf("%s%s=%s&", endpoint, gk, gv)
+			}
+
+			var r io.Reader = strings.NewReader(wu.r.Body)
+			doRequest(endpoint, r, tr)
+
+			continue
+		}
+
+	}
+
+	wg.Done()
+
 }
+
+func doRequest(endpoint string, body io.Reader, tr *http.Transport) {}
