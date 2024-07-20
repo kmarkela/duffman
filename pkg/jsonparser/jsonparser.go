ad223d1fbdd1a4f14eb42a3910d64ca1b066835d
Kanstantsin Markelau <48592947+kmarkela@users.noreply.github.com>
2024-07-20 11:23:12 +0200

    unmarshal

--- pkg/jsonparser/jsonparser.go
+++ pkg/jsonparser/jsonparser.go
@@ -6,41 +6,85 @@ import (
 	"strings"
 )
 
-// |_!$+
-// |)31![V]37lz
+const (
+	delimiter = ".._=.dl.=_.."
+	slice     = "|_!$+"
+)
 
-const delimiter = ".._=.dl.=_.."
-
-func Unmarshal(s string) (map[string]string, error) {
-
-	var data map[string]interface{} = make(map[string]interface{})
-	var dataList []map[string]interface{}
-	var result map[string]string
-
-	switch {
-	case strings.HasPrefix(s, "["):
-		if err := json.Unmarshal([]byte(s), &dataList); err != nil {
-			return result, fmt.Errorf("error parsing JSON: %s", err.Error())
-		}
-		// TODO: Fuzzing only firs element in the list. update to fuzz all
-		data = dataList[0]
-		data["DM-data-in-slice"] = struct{}{}
-	default:
-		if err := json.Unmarshal([]byte(s), &data); err != nil {
-			return result, fmt.Errorf("error parsing JSON: %s", err.Error())
-		}
+func Unmarshal(input string) (map[string]string, error) {
+	var data interface{}
+	if err := json.Unmarshal([]byte(input), &data); err != nil {
+		return nil, err
 	}
-
-	result = parseJSONBody(data, "")
-
+	result := make(map[string]string)
+	parseJSON(data, "", result)
 	return result, nil
 }
 
-func parseSlice(data []interface{}, prefix string) map[string]string {
-	for i, v := range data {
-
+func parseJSON(data interface{}, prefix string, result map[string]string) {
+	switch value := data.(type) {
+	case map[string]interface{}:
+		for k, v := range value {
+			parseJSON(v, prefix+k+delimiter, result)
+		}
+	case []interface{}:
+		for i, v := range value {
+			parseJSON(v, fmt.Sprintf("%s%d%s", prefix, i, delimiter), result)
+		}
+	default:
+		result[prefix[:len(prefix)-1]] = interface2str(value)
 	}
 }
+
+type convertible interface {
+	~int | ~float64 | ~string | ~bool | interface{}
+}
+
+func interface2str[T convertible](value T) string {
+	switch v := any(value).(type) {
+	case string:
+		return v
+	case int:
+		return fmt.Sprintf("%d", v)
+	case float64:
+		return fmt.Sprintf("%f", v)
+	case bool:
+		return fmt.Sprintf("%t", v)
+	default:
+		return fmt.Sprintf("%v", v)
+	}
+}
+
+// func Unmarshal(s string) (map[string]string, error) {
+
+// 	var data map[string]interface{} = make(map[string]interface{})
+// 	var dataList []map[string]interface{}
+// 	var result map[string]string
+
+// 	switch {
+// 	case strings.HasPrefix(s, "["):
+// 		if err := json.Unmarshal([]byte(s), &dataList); err != nil {
+// 			return result, fmt.Errorf("error parsing JSON: %s", err.Error())
+// 		}
+// 		// TODO: Fuzzing only firs element in the list. update to fuzz all
+// 		data = dataList[0]
+// 		data["DM-data-in-slice"] = struct{}{}
+// 	default:
+// 		if err := json.Unmarshal([]byte(s), &data); err != nil {
+// 			return result, fmt.Errorf("error parsing JSON: %s", err.Error())
+// 		}
+// 	}
+
+// 	result = parseJSONBody(data, "")
+
+// 	return result, nil
+// }
+
+// func parseSlice(data []interface{}, prefix string) map[string]string {
+// 	for i, v := range data {
+
+// 	}
+// }
 
 func parseJSONBody(data map[string]interface{}, prefix string) map[string]string {
 	result := make(map[string]string)
