1e13fde819c7e986628f56b563f66a17a09e1cf7
Kanstantsin Markelau <48592947+kmarkela@users.noreply.github.com>
2024-08-30 15:50:54 +0200

    inter

---
+++ internal/interactive/inter.go
@@ -0,0 +1,36 @@
+package interactive
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/kmarkela/duffman/internal/pcollection"
+)
+
+type Inter struct {
+	tr *http.Transport
+}
+
+func New(proxy string) (Inter, error) {
+
+	var inter = Inter{}
+	var tr = http.Transport{}
+
+	if proxy != "" {
+		// parse proxy
+		proxyUrl, err := url.Parse(proxy)
+		if err != nil {
+			return inter, err
+		}
+
+		tr = http.Transport{Proxy: http.ProxyURL(proxyUrl)}
+
+	}
+
+	inter.tr = &tr
+
+	return inter, nil
+
+}
+
+func (i *Inter) Run(col *pcollection.Collection) {}
