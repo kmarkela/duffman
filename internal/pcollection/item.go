e2775385f9e2ab148b04216c138dfbd3488f43a3
Kanstantsin Markelau <48592947+kmarkela@users.noreply.github.com>
2024-09-02 17:55:16 +0200

    schema

--- internal/pcollection/item.go
+++ internal/pcollection/item.go
@@ -15,6 +15,8 @@ func (i *Item) i2ReqLt(n *Node) ([]Req, error) {
 
 	for _, v := range i.Item {
 		tn := Node{}
+		// fmt.Println(v.Name)
+		tn.Name = v.Name
 		tr, err := v.i2ReqLt(&tn)
 		if err != nil {
 			return nil, err
