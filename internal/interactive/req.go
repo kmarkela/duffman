7618a563d40660e40254c22fd81b6686471ce957
Kanstantsin Markelau <48592947+kmarkela@users.noreply.github.com>
2024-09-09 16:39:00 +0200

    output1

--- internal/interactive/req.go
+++ internal/interactive/req.go
@@ -1,22 +1,70 @@
 package interactive
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 
 	"github.com/kmarkela/duffman/internal/pcollection"
 )
 
+// func buildString(r pcollection.Req) string {
+// 	out := fmt.Sprintf("Method:: %s\n", r.Method)
+// 	out += fmt.Sprintf("URL:: %s\n", r.URL)
+// 	if len(r.Headers) > 0 {
+// 		out += "HEADERS:: "
+
+// 		for k, v := range r.Headers {
+// 			out += fmt.Sprintf("%s: %s\n", k, v)
+// 		}
+// 	}
+
+// 	return out
+
+// }
+
 func buildString(r pcollection.Req) string {
-	out := fmt.Sprintf("Method:: %s\n", r.Method)
-	out += fmt.Sprintf("URL:: %s\n", r.URL)
-	if len(r.Headers) > 0 {
-		out += "HEADERS:: "
-
-		for k, v := range r.Headers {
-			out += fmt.Sprintf("%s: %s\n", k, v)
-		}
+	// r.Parameters = nil
+	marshaled, err := json.MarshalIndent(r, "", "   ")
+	if err != nil {
+		log.Fatalf("marshaling error: %s", err)
 	}
-
-	return out
+	return string(marshaled)
 
 }
+
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+// )
+
+// type Teacher struct {
+// 	ID        string  `json:"id"`
+// 	Firstname string  `json:"firstname"`
+// 	Lastname  string  `json:"lastname"`
+// 	TT        Test123 `json:"tt"`
+// }
+
+// type Test123 struct {
+// 	T1 string `json:"T1"`
+// 	T2 string `json:"t2"`
+// }
+
+// func main() {
+
+// 	tt := Test123{
+// 		T1: "asdasd",
+// 		T2: "m,kiuyhnmju",
+// 	}
+// 	john := Teacher{
+// 		ID:        "678930",
+// 		Firstname: "John",
+// 		Lastname:  "Doe",
+// 		TT:        tt,
+// 	}
+// 	marshaled, err := json.MarshalIndent(john, "", "   ")
+// 	if err != nil {
+// 		log.Fatalf("marshaling error: %s", err)
+// 	}
+// 	fmt.Println(string(marshaled))
+// }
