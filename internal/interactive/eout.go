3b09351b81daf39807c37299f7fcf16fd2039397
Kanstantsin Markelau <48592947+kmarkela@users.noreply.github.com>
2024-09-12 12:41:54 +0200

    vars

--- internal/interactive/eout.go
+++ internal/interactive/eout.go
@@ -2,17 +2,19 @@ package interactive
 
 import (
 	"encoding/json"
-	"log"
-
-	// "log"
 
 	"github.com/kmarkela/duffman/internal/pcollection"
 )
 
+type varOut struct {
+	Variables []pcollection.KeyValue `json:"Variables,omitempty"`
+	Env       []pcollection.KeyValue `json:"Enviroment,omitempty"`
+}
+
 func buildReqStr(r pcollection.Req) string {
 	marshaled, err := json.MarshalIndent(r, "", "   ")
 	if err != nil {
-		log.Fatalf("marshaling error: %s", err)
+		return err.Error()
 	}
 	return string(marshaled)
 
@@ -20,48 +22,15 @@ func buildReqStr(r pcollection.Req) string {
 
 func buildVarStr(col pcollection.Collection) string {
 
-	marshaled, err := json.MarshalIndent(col, "", "   ")
+	var vo varOut
+
+	vo.Variables = col.Variables
+	vo.Env = col.Env
+
+	marshaled, err := json.MarshalIndent(vo, "", "   ")
 	if err != nil {
-		log.Fatalf("marshaling error: %s", err)
+		return err.Error()
 	}
-	// logger.Logger.Info(string(marshaled))
 	return string(marshaled)
 
 }
-
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"log"
-// )
-
-// type Teacher struct {
-// 	ID        string  `json:"id"`
-// 	Firstname string  `json:"firstname"`
-// 	Lastname  string  `json:"lastname"`
-// 	TT        Test123 `json:"tt"`
-// }
-
-// type Test123 struct {
-// 	T1 string `json:"T1"`
-// 	T2 string `json:"t2"`
-// }
-
-// func main() {
-
-// 	tt := Test123{
-// 		T1: "asdasd",
-// 		T2: "m,kiuyhnmju",
-// 	}
-// 	john := Teacher{
-// 		ID:        "678930",
-// 		Firstname: "John",
-// 		Lastname:  "Doe",
-// 		TT:        tt,
-// 	}
-// 	marshaled, err := json.MarshalIndent(john, "", "   ")
-// 	if err != nil {
-// 		log.Fatalf("marshaling error: %s", err)
-// 	}
-// 	fmt.Println(string(marshaled))
-// }
